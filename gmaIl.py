de87ad238dafedd8d288e2901127e660b5418b0e
matin nouriyan manesh <71287596+matitanium@users.noreply.github.com>
2021-08-02 04:35:44 -0700

    Update gmaIl.py

--- gmaIl.py
+++ gmaIl.py
@@ -4,7 +4,7 @@ user = "matinnoriyan@gmail.com" #your email
 passw = "******" #your password
 #code by matin nouriyan
 froM = user
-to  = "target@gmail.com" #target
+to  = ["target@gmail.com"] #target
 message = "hello matin "
 smtp = smtplib.SMTP()
 smtp.connect("smtp.gmail.com",587)
